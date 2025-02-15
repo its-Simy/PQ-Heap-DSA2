33569ca2c7fd9cd9900371e1ab140c4d3c5f526f
its-Simy <135281067+its-Simy@users.noreply.github.com>
2025-02-14 23:05:48 -0500

    Main implemention
    Implemented some useful file reading loops, but not fully completed.

--- Main.java
+++ Main.java
@@ -1,5 +1,33 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        PQHeap pq = new PQHeap();
 
+        try {
+            FileReader fileReader = new FileReader("info.txt");
+            Scanner infile = new Scanner(fileReader);
+            int counter = 0;
+            String name;
+            int score;
+
+
+            while(infile.hasNextLine()) {
+                name = infile.nextLine();
+                counter++;
+                if(counter % 2 != 0){
+                    score = Integer.parseInt(infile.nextLine());
+                    counter++;
+                    Player p = new Player(name, score);
+                    //pq.add(p);
+                }
+            }
+            infile.close();
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
