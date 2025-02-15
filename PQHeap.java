7845b69a5898788ee4fb214e6c038c8befbf8457
its-Simy <135281067+its-Simy@users.noreply.github.com>
2025-02-15 13:30:28 -0500

    Implementation
    Implemented default constructor, interface methods, logic for the getSize() and isEmpty() methods.

--- PQHeap.java
+++ PQHeap.java
@@ -1,2 +1,52 @@
-public class PQHeap {
+public class PQHeap implements PriorityQueue{
+    private int[] heap;
+    private int size;
+
+    /**
+     * Default Constructor
+     */
+    public PQHeap(){
+        heap = new int[100];
+        size = 0;
+    }
+
+    /**
+     * Adds a player to the end of the Queue
+     */
+    @Override
+    public void add(Player a) {
+
+    }
+
+    /**
+     *Will return the player with the highest score in the queue
+     */
+    @Override
+    public Player getHighestScorePlayer() {
+        return null;
+    }
+
+    /**
+     * Will remove all the players within the queue
+     */
+    @Override
+    public void clear() {
+        
+    }
+
+    /**
+     *Will return the size (not index) of the queue
+     */
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * Will return true if the queue is empty, false if it is not
+     */
+    @Override
+    public boolean isEmpty() {
+        return (size == 0);
+    }
 }
